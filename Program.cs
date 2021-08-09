b62066c29c607fa96ab6c883fa62ca0a85079b5f
KomischerBoy <info@versahl.at>
2021-08-09 19:27:24 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ASCII_Raw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> lines = new List<string[]>();
+            string[] rows = { " id ", " name ", " surename " };
+            string[] line1 = { " 1 ", " George ", " Gross " };
+            string[] line2 = { " 2 ", " Edie ", " Kirk " };
+            string[] line3 = { " 3 ", " Wilda ", " Simon " };
+            lines.Add(rows);
+            lines.Add(line1);
+            lines.Add(line2);
+            lines.Add(line3);
+
+            ASCIITable table = new ASCIITable(lines);
+            System.Console.WriteLine(table.GetAsString());
+        }
+    }
+}
